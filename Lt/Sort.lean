51fdb080d5fd8931873b9811ea339867dc6d5612
Gregor Kikelj <gregor1234567890@gmail.com>
2023-12-30 13:44:05 +0100

    dokazemo insertion sort

--- Lt/Sort.lean
+++ Lt/Sort.lean
@@ -7,7 +7,6 @@ def insertElemIntoSortedList (as: List Nat) (b: Nat) : List Nat :=
 
 #eval insertElemIntoSortedList [1, 3, 4, 8, 9] 10
 
-
 theorem insertIntoSortedIsSorted (as: List Nat) (b:Nat) (h: is_sorted as):
   is_sorted (insertElemIntoSortedList as b) := by
   induction as with
@@ -38,3 +37,30 @@ theorem insertIntoSortedIsSorted (as: List Nat) (b:Nat) (h: is_sorted as):
         · simp [h3] at ih ⊢
           unfold is_sorted
           simp [h.left, ih]
+
+
+def insertionSortAux (as sorted: List Nat) : List Nat :=
+  match as with
+  | [] => sorted
+  | a::as' => insertionSortAux as' (insertElemIntoSortedList sorted a)
+
+def insertionSort (as: List Nat) : List Nat :=
+  insertionSortAux as []
+
+#eval insertionSort [1,5,2,2,4]
+
+theorem aux (as bs:List Nat) : is_sorted bs → is_sorted (insertionSortAux as bs) := by
+  intro h
+  induction as generalizing bs with
+  | nil => simp [is_sorted, insertionSortAux, h]
+  | cons a as' ih =>
+    unfold insertionSortAux
+    exact ih (insertElemIntoSortedList bs a) (insertIntoSortedIsSorted _ _ h)
+
+theorem isertionSortSorts (as: List Nat) : is_sorted (insertionSort as) := by
+  unfold insertionSort insertionSortAux
+  match as with
+  | [] => rfl
+  | a::as' =>
+    apply aux
+    simp [insertElemIntoSortedList, is_sorted]
